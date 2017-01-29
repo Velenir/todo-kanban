cfdf0e02973dec31d90cd316a72a87b343340d5e
Velenir <Velenir@users.noreply.github.com>
2017-01-29 16:54:22 +0300

    Simplify reducers

--- app/reducers/todoReducer.js
+++ app/reducers/todoReducer.js
@@ -1,74 +1,61 @@
 import {List, Map} from 'immutable';
 
-function findItemIndex(list, itemId) {
-	return list.findIndex(
-		(item) => item.get('id') === itemId
-	);
-}
 
-function toggleComplete(state, {itemId}) {
-	const itemIndex = findItemIndex(state, itemId);
-
-	return state.updateIn([itemIndex, "status"], status => status === 'active' ? 'completed' : 'active');
-}
-
-function editItem(state, {itemId}) {
-	const itemIndex = findItemIndex(state, itemId);
+function changeItemMap(action) {
+	const {type, itemId} = action;
 	
-	return state.mergeIn([itemIndex], {editing: true, selectText: false});
+	switch (type) {
+		case "TOGGLE_COMPLETE":
+			return item => item.get("id") === itemId ? item.update("status",
+				status => status === "active" ? "completed" : "active")
+			: item;
+		case "EDIT_ITEM":
+			return item => item.get("id") === itemId ? item.merge({editing: true, selectText: false})
+			: item;
+		case "SELECT_EDIT_ITEM":
+			return item => item.get("id") === itemId ? item.merge({editing: true, selectText: true})
+			: item;
+		case "CANCEL_EDITING":
+			return item => item.get("id") === itemId ? item.set("editing", false)
+			: item;
+		case "DONE_EDITING":
+			return item => item.get("id") === itemId ? item.merge({editing: false, text: action.newText})
+			: item;
+		default:
+			return item => item;
+	}
 }
 
-function selectAndEditItem(state, {itemId}) {
-	const itemIndex = findItemIndex(state, itemId);
-	
-	return state.mergeIn([itemIndex], {editing: true, selectText: true});
+function deleteItemCondition(action) {
+	switch (action.type) {
+		case "CLEAR_COMPLETED":
+			return item => item.get("status") === "completed";
+		case "DELETE_ITEM":
+			return item => item.get("id") === action.itemId;
+		default:
+			return () => false;
+	}
 }
 
-function cancelEditing(state, {itemId}) {
-	const itemIndex = findItemIndex(state, itemId);
-	
-	return state.setIn([itemIndex, "editing"], false);
-}
-
-function doneEditing(state, {itemId, newText}) {
-	const itemIndex = findItemIndex(state, itemId);
-
-	return state.mergeIn([itemIndex], {editing: false, text: newText});
-}
-
-function clearCompleted(state) {
-	return state.filterNot(item => item.get("status") === "completed");
-}
-
-function addItem(state, {text}) {
+function createNewItem(state, action) {
 	const itemId = state.reduce((maxId, item) => Math.max(maxId, item.get("id")), 0) + 1;
 
-	const newItem = Map({id: itemId, text: text, status: "active"});
-	return state.push(newItem);
-}
-
-function deleteItem(state, {itemId}) {
-	return state.filterNot(item => item.get("id") === itemId);
+	return Map({id: itemId, text: action.text, status: "active"});
 }
 
 export default function (state = List(), action) {
 	switch (action.type) {
 		case "TOGGLE_COMPLETE":
-			return toggleComplete(state, action);
-		case 'EDIT_ITEM':
-			return editItem(state, action);
-		case 'SELECT_EDIT_ITEM':
-			return selectAndEditItem(state, action);
-		case 'CANCEL_EDITING':
-			return cancelEditing(state, action);
-		case 'DONE_EDITING':
-			return doneEditing(state, action);
-		case 'CLEAR_COMPLETED':
-			return clearCompleted(state);
-		case 'ADD_ITEM':
-			return addItem(state, action);
-		case 'DELETE_ITEM':
-			return deleteItem(state, action);
+		case "EDIT_ITEM":
+		case "SELECT_EDIT_ITEM":
+		case "CANCEL_EDITING":
+		case "DONE_EDITING":
+			return state.map(changeItemMap(action));
+		case "CLEAR_COMPLETED":
+		case "DELETE_ITEM":
+			return state.filterNot(deleteItemCondition(action));
+		case "ADD_ITEM":
+			return state.push(createNewItem(state, action));
 		default:
 			return state;
 	}
