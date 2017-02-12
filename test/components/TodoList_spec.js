3a4526036505d315d7a8b2ec6c4ac8d97ff1b623
Velenir <Velenir@users.noreply.github.com>
2017-02-12 17:20:28 +0300

    Add DnD test for TodoList

--- test/components/TodoList_spec.js
+++ test/components/TodoList_spec.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {renderIntoDocument, scryRenderedDOMComponentsWithTag} from 'react-addons-test-utils';
+import {renderIntoDocument, scryRenderedDOMComponentsWithTag, scryRenderedComponentsWithType} from 'react-addons-test-utils';
 import DropTodoList from '../../app/components/TodoList';
+import DragTodoItem from '../../app/components/TodoItem';
 import TestBackend from 'react-dnd-test-backend';
 import { DragDropContext } from 'react-dnd';
 import {expect} from 'chai';
-import {filterTodos, fromJS} from '../../app/helpers/immutableHelpers';
+import {filterTodos, fromJS, findItemEntry} from '../../app/helpers/immutableHelpers';
 import * as FILTER from '../../app/reducers/filterVars';
 
 describe('TodoList', () => {
@@ -67,5 +68,164 @@ describe('TodoList', () => {
 		expect(items[0].textContent).to.contain('React');
 		expect(items[1].textContent).to.contain('Redux');
 		expect(items[2].textContent).to.contain('Immutable');
+	});
+	
+	it('should find item entry on beginDrag', () => {
+		const todos = fromJS([
+			{id: 1, text: 'React', status: FILTER.ACTIVE},
+			{id: 2, text: 'Redux', status: FILTER.ACTIVE},
+			{id: 3, text: 'Immutable', status: FILTER.ACTIVE}
+			]),
+			filter = FILTER.ALL;
+		
+		let foundItem = null;
+		// Keep track of findItem function execution
+		const findItem = (itemId) => foundItem = findItemEntry(todos, itemId);
+		// Stub moveItem function
+		const moveItem = () => {};
+		
+		const component = renderIntoDocument(
+			<TodoList filter={filter} todos={todos} findItem={findItem} moveItem={moveItem}/>
+		);
+		const backend = component.getManager().getBackend();
+		
+		const todoItems = scryRenderedComponentsWithType(component, DragTodoItem);
+		
+		backend.simulateBeginDrag([todoItems[2].getHandlerId()]);
+
+		expect(foundItem).to.deep.equal([2, todos.get(2)]);
+		
+		backend.simulateEndDrag();
+	});
+	
+	it('should move item on hover when dragging', () => {
+		const todos = fromJS([
+			{id: 1, text: 'React', status: FILTER.ACTIVE},
+			{id: 2, text: 'Redux', status: FILTER.ACTIVE},
+			{id: 3, text: 'Immutable', status: FILTER.ACTIVE}
+			]),
+			filter = FILTER.ALL;
+		
+		// Implement findItem function
+		const findItem = (itemId) => findItemEntry(todos, itemId);
+		
+		let moveArguments = null;
+		// Keep track of moveItem function execution
+		const moveItem = (draggedEntry, overIndex) => moveArguments = {draggedEntry, overIndex};
+		
+		const component = renderIntoDocument(
+			<TodoList filter={filter} todos={todos} findItem={findItem} moveItem={moveItem}/>
+		);
+		const backend = component.getManager().getBackend();
+		
+		const todoItems = scryRenderedComponentsWithType(component, DragTodoItem);
+		
+		backend.simulateBeginDrag([todoItems[2].getHandlerId()]);
+		// hover over DropTarget-wrapped instance
+		backend.simulateHover([todoItems[0].getDecoratedComponentInstance().getHandlerId()]);
+		
+		expect(moveArguments).to.deep.equal({draggedEntry: [2, todos.get(2)], overIndex: 0});
+		
+		backend.simulateEndDrag();
+	});
+	
+	it('should not move item on hover over item itself when dragging', () => {
+		const todos = fromJS([
+			{id: 1, text: 'React', status: FILTER.ACTIVE},
+			{id: 2, text: 'Redux', status: FILTER.ACTIVE},
+			{id: 3, text: 'Immutable', status: FILTER.ACTIVE}
+			]),
+			filter = FILTER.ALL;
+		
+		// Implement findItem function
+		const findItem = (itemId) => findItemEntry(todos, itemId);
+		
+		let moveArguments = null;
+		// Keep track of moveItem function execution
+		const moveItem = (draggedEntry, overIndex) => moveArguments = {draggedEntry, overIndex};
+		
+		const component = renderIntoDocument(
+			<TodoList filter={filter} todos={todos} findItem={findItem} moveItem={moveItem}/>
+		);
+		const backend = component.getManager().getBackend();
+		
+		const todoItems = scryRenderedComponentsWithType(component, DragTodoItem);
+		
+		backend.simulateBeginDrag([todoItems[2].getHandlerId()]);
+		// hover over DropTarget-wrapped instance of itself
+		backend.simulateHover([todoItems[2].getDecoratedComponentInstance().getHandlerId()]);
+		
+		// Verify that item wasn't moved
+		expect(moveArguments).to.be.null;
+		
+		backend.simulateEndDrag();
+	});
+	
+	it('should retain item in its position from last hover when dropped in valid location', () => {
+		const todos = fromJS([
+			{id: 1, text: 'React', status: FILTER.ACTIVE},
+			{id: 2, text: 'Redux', status: FILTER.ACTIVE},
+			{id: 3, text: 'Immutable', status: FILTER.ACTIVE}
+			]),
+			filter = FILTER.ALL;
+		
+		// Implement findItem function
+		const findItem = (itemId) => findItemEntry(todos, itemId);
+		
+		let moveArguments = null;
+		// Keep track of moveItem function execution
+		const moveItem = (draggedEntry, overIndex) => moveArguments = {draggedEntry, overIndex};
+		
+		const component = renderIntoDocument(
+			<TodoList filter={filter} todos={todos} findItem={findItem} moveItem={moveItem}/>
+		);
+		const backend = component.getManager().getBackend();
+		
+		const todoItems = scryRenderedComponentsWithType(component, DragTodoItem);
+		
+		backend.simulateBeginDrag([todoItems[2].getHandlerId()]);
+		// hover over DropTarget-wrapped instance
+		backend.simulateHover([todoItems[0].getDecoratedComponentInstance().getHandlerId()]);
+		
+		// to index 0 on hover
+		expect(moveArguments).to.deep.equal({draggedEntry: [2, todos.get(2)], overIndex: 0});
+		backend.simulateDrop();
+		
+		// stay at index 0 on endDrag
+		expect(moveArguments).to.deep.equal({draggedEntry: [2, todos.get(2)], overIndex: 0});
+		backend.simulateEndDrag();
+	});
+	
+	it('should return item back to its original position when dropped not in valid location', () => {
+		const todos = fromJS([
+			{id: 1, text: 'React', status: FILTER.ACTIVE},
+			{id: 2, text: 'Redux', status: FILTER.ACTIVE},
+			{id: 3, text: 'Immutable', status: FILTER.ACTIVE}
+			]),
+			filter = FILTER.ALL;
+		
+		// Implement findItem function
+		const findItem = (itemId) => findItemEntry(todos, itemId);
+		
+		let moveArguments = null;
+		// Keep track of moveItem function execution
+		const moveItem = (draggedEntry, overIndex) => moveArguments = {draggedEntry, overIndex};
+		
+		const component = renderIntoDocument(
+			<TodoList filter={filter} todos={todos} findItem={findItem} moveItem={moveItem}/>
+		);
+		const backend = component.getManager().getBackend();
+		
+		const todoItems = scryRenderedComponentsWithType(component, DragTodoItem);
+		
+		backend.simulateBeginDrag([todoItems[2].getHandlerId()]);
+		// hover over DropTarget-wrapped instance
+		backend.simulateHover([todoItems[0].getDecoratedComponentInstance().getHandlerId()]);
+		
+		// to index 0 on hover
+		expect(moveArguments).to.deep.equal({draggedEntry: [2, todos.get(2)], overIndex: 0});
+		backend.simulateEndDrag();
+		// back to index 2 on endDrag
+		expect(moveArguments).to.deep.equal({draggedEntry: [2, todos.get(2)], overIndex: 2});
 	});
 });
