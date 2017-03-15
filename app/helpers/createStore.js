ad3d4230ba1c8bd7b527a894e6ba455003545c52
Velenir <Velenir@users.noreply.github.com>
2017-03-15 21:53:38 +0300

    Separate off store creation

---
+++ app/helpers/createStore.js
@@ -0,0 +1,45 @@
+import {createStore} from 'redux';
+import reducer from '../reducers';
+import {fromJS, ListRecord, List} from './immutableHelpers';
+// import {List} from 'immutable';
+
+import {v4 as uuid} from 'uuid';
+
+function getDefaultInitialState() {
+	return {
+		lists: List.of(
+			ListRecord({
+				id: uuid(),
+				title: "Technologies used",
+				todos: fromJS([
+					{id: uuid(), text: 'React'},
+					{id: uuid(), text: 'Redux'},
+					{id: uuid(), text: 'Immutable'}
+				])
+			}),
+			ListRecord({
+				id: uuid(),
+				title: "New List",
+				todos: fromJS([
+					{id: uuid(), text: 'Item 1'},
+					{id: uuid(), text: 'Item 2'},
+					{id: uuid(), text: 'Item 3'},
+					{id: uuid(), text: 'Item 4'}
+				])
+			})
+		)
+	};
+}
+
+export default function createMyStore(initialState = getDefaultInitialState()) {
+	// eslint-disable-next-line no-underscore-dangle
+	return createStore(reducer, initialState, process.env.NODE_ENV !== 'production' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({
+		serialize: {
+			options: {
+				symbol: true
+			},
+			immutable: require('immutable'),
+			refs: [require('./immutableHelpers').TodoRecord]
+		}
+	}) : undefined);
+}
--- app/index.jsx
+++ app/index.jsx
@@ -1,53 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import reducer from './reducers';
-import {fromJS, ListRecord, List} from './helpers/immutableHelpers';
-// import {List} from 'immutable';
-
-import {v4 as uuid} from 'uuid';
+import createMyStore from './helpers/createStore';
 
 import 'todomvc-app-css/index.css';
 import './scss/style.scss';
 
 import Board from './containers/BoardContainer';
 
-const initialState = {
-	lists: List.of(
-		ListRecord({
-			id: uuid(),
-			title: "Technologies used",
-			todos: fromJS([
-				{id: uuid(), text: 'React'},
-				{id: uuid(), text: 'Redux'},
-				{id: uuid(), text: 'Immutable'}
-			])
-		}),
-		ListRecord({
-			id: uuid(),
-			title: "New List",
-			todos: fromJS([
-				{id: uuid(), text: 'Item 1'},
-				{id: uuid(), text: 'Item 2'},
-				{id: uuid(), text: 'Item 3'},
-				{id: uuid(), text: 'Item 4'}
-			])
-		})
-	)
-};
-
-// eslint-disable-next-line no-underscore-dangle
-const store = createStore(reducer, initialState, process.env.NODE_ENV !== 'production' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({
-	serialize: {
-		options: {
-			symbol: true
-		},
-		immutable: require('immutable'),
-		refs: [require('./helpers/immutableHelpers').TodoRecord]
-	}
-}) : undefined);
+const store = createMyStore();
 
 render(
 	<Provider store={store}>
