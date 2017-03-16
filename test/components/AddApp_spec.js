d06a6cf5f8f62a8b4d653e007d3461222a1a4194
Velenir <Velenir@users.noreply.github.com>
2017-03-16 22:30:27 +0300

    Add AddApp tests

---
+++ test/components/AddApp_spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {renderIntoDocument, findRenderedDOMComponentWithTag, Simulate} from 'react-addons-test-utils';
+import AddApp from '../../app/components/AddApp';
+import {expect} from 'chai';
+
+describe('AddApp', () => {
+	it('should invoke a callback when button is clicked', () => {
+		let called = null;
+		// mock addList function
+		const addList = () => called = true;
+		
+		const component = renderIntoDocument(
+			<AddApp lists={[]} addList={addList}/>
+		);
+		
+		const button = findRenderedDOMComponentWithTag(component, "button");
+		Simulate.click(button);
+		
+		expect(called).to.be.true;
+	});
+});
