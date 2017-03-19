fad48105964c4838e39d92e12cbd75324307e5b2
Velenir <Velenir@users.noreply.github.com>
2017-03-19 16:16:39 +0300

    Make CheckBox a controlled component

--- app/components/Checkbox.jsx
+++ app/components/Checkbox.jsx
@@ -6,25 +6,32 @@ class CheckBox extends PureComponent {
 	}
 	
 	onChange = (e) => {
-		this.setState({
-			checked: e.target.checked
-		});
+		const {onChange, checked} = this.props;
 		
-		this.props.onChange && this.props.onChange(e);
+		// if CheckBox isn't controlled
+		if(checked !== undefined) {
+			this.setState({
+				checked: e.target.checked
+			});
+		}
+		
+		onChange && onChange(e);
 	}
 	
 	render() {
 		const {
-			className = "",
+			className,
 			whenChecked = <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="-10 -10 120 120" style={{verticalAlign: "top"}}><circle cx="50" cy="50" r="50" fill="none" stroke="#bddad5" strokeWidth="3"/><path fill="#5dc2af" d="M72 25L42 71 27 56l-4 4 20 20 34-52z"/></svg>,
 			whenUnchecked = <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="-10 -10 120 120" style={{verticalAlign: "top"}}><circle cx="50" cy="50" r="50" fill="none" stroke="#ededed" strokeWidth="3"/></svg>,
+			checked = this.state.checked,
+			style,
 			...checkboxProps
 		} = this.props;
 				
 		return (
-			<label className={className}>
-				<input type="checkbox" {...checkboxProps} onChange={this.onChange} style={{display: "none"}}/>
-				{this.state.checked ? whenChecked : whenUnchecked}
+			<label className={className} style={style}>
+				<input type="checkbox" checked={checked} {...checkboxProps} onChange={this.onChange} style={{display: "none"}}/>
+				{checked ? whenChecked : whenUnchecked}
 			</label>
 		);
 	}
