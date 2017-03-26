462035ffd99b1894cb4efbadd79eb67431a699d0
Velenir <Velenir@users.noreply.github.com>
2017-03-26 17:15:02 +0300

    Don't recalculate TextArea's heightOffset unnecessarily

--- app/components/TextArea.jsx
+++ app/components/TextArea.jsx
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 
 class TextArea extends Component {
+	// for our use case consider TextArea style constant
+	static heightOffset = null;
+	
 	resize() {
 		const textarea = this.textarea;
 		const style = textarea.style;
 		style.height = "auto";
-		style.height = textarea.scrollHeight + this.heightOffset + "px";
+		style.height = textarea.scrollHeight + TextArea.heightOffset + "px";
 	}
 	onChange = (e) => {
 		this.resize();
@@ -30,17 +33,20 @@ class TextArea extends Component {
 	
 	componentDidMount() {
 		console.log("TextArea MOUNTED");
-		const {paddingBottom, paddingTop, boxSizing, borderTopWidth, borderBottomWidth} = window.getComputedStyle(this.textarea);
-		
-		let heightOffset;
-		if(boxSizing === "border-box") heightOffset = parseFloat(borderTopWidth) + parseFloat(borderBottomWidth);
-		else {
-			heightOffset = parseFloat(paddingTop) + parseFloat(paddingBottom);
-			if(boxSizing === "content-box") heightOffset *= -1;
+		if(TextArea.heightOffset === null) {
+			const {paddingBottom, paddingTop, boxSizing, borderTopWidth, borderBottomWidth} = window.getComputedStyle(this.textarea);
+			
+			let heightOffset;
+			if(boxSizing === "border-box") heightOffset = parseFloat(borderTopWidth) + parseFloat(borderBottomWidth);
+			else {
+				heightOffset = parseFloat(paddingTop) + parseFloat(paddingBottom);
+				if(boxSizing === "content-box") heightOffset *= -1;
+			}
+			
+			TextArea.heightOffset = heightOffset;
+			console.log("Calculated heightOffset =", heightOffset);
 		}
 		
-		// TODO: consider making heaightOffset static
-		this.heightOffset = heightOffset;
 		this.resize();
 	}
 }
