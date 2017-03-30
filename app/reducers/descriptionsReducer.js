461cc7c4759aef659c2dad09a78a591dd8d06a3c
Velenir <Velenir@users.noreply.github.com>
2017-03-30 21:50:53 +0300

    Delete description with the item it belongs to

--- app/reducers/descriptionsReducer.js
+++ app/reducers/descriptionsReducer.js
@@ -1,10 +1,12 @@
 import {Map} from 'immutable';
-import {CHANGE_DESCRIPTION} from '../actions/actionTypes';
+import {CHANGE_DESCRIPTION, DELETE_ITEM} from '../actions/actionTypes';
 
 export default function (state = Map(), action) {
 	switch (action.type) {
 		case CHANGE_DESCRIPTION:
 			return state.set(action.itemId, action.description);
+		case DELETE_ITEM:
+			return state.delete(action.itemId);
 		default:
 			return state;
 	}
