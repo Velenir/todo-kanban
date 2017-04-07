8884e45e4712e50b5f64304454285ae1e3fda24e
Velenir <Velenir@users.noreply.github.com>
2017-04-07 13:00:19 +0300

    Add checked state class

--- app/components/Checkbox.jsx
+++ app/components/Checkbox.jsx
@@ -25,11 +25,12 @@ class CheckBox extends PureComponent {
 			whenUnchecked = <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="-10 -10 120 120" style={{verticalAlign: "top"}}><circle cx="50" cy="50" r="50" fill="none" stroke="#ededed" strokeWidth="3"/></svg>,
 			checked = this.state.checked,
 			style,
+			checkedClass = "checked",
 			...checkboxProps
 		} = this.props;
 				
 		return (
-			<label className={className} style={style}>
+			<label className={checked ? className ? `${className} ${checkedClass}` : checkedClass : className} style={style}>
 				<input type="checkbox" checked={checked} {...checkboxProps} onChange={this.onChange} style={{display: "none"}}/>
 				{checked ? whenChecked : whenUnchecked}
 			</label>
