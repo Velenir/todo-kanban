2d127e43ed4da55d06bfe6413c4f9fd56949d516
Velenir <Velenir@users.noreply.github.com>
2017-04-07 16:57:33 +0300

    Add icons to Header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,6 +3,14 @@ import Radio from './Checkbox';
 
 import '../styles/header.scss';
 
+import LayoutColumnWrap from '../images/layout-flex-column-wrap.svg';
+import LayoutColumn from '../images/layout-flex-column.svg';
+import LayoutRow from '../images/layout-flex-row.svg';
+
+const ColumnWrap = <LayoutColumnWrap width={undefined} height={undefined}/>;
+const Column = <LayoutColumn width={undefined} height={undefined}/>;
+const Row = <LayoutRow width={undefined} height={undefined}/>;
+
 class Header extends Component {
 	state = {checked: -1}
 	
@@ -16,10 +24,9 @@ class Header extends Component {
 	render() {
 		return (
 			<header className="main-header" onChange={this.onChange}>
-				<Radio checked={this.state.checked === 0} type="radio" name="layout" whenChecked="⦿" whenUnchecked="⦾" value={0} className="layout-radio"/>
-				<Radio checked={this.state.checked === 1} type="radio" name="layout" whenChecked="⦿" whenUnchecked="⦾" value={1} className="layout-radio"/>
-				<Radio checked={this.state.checked === 2} type="radio" name="layout" whenChecked="⦿" whenUnchecked="⦾" value={2} className="layout-radio"/>
-				<Radio checked={this.state.checked === 3} type="radio" name="layout" whenChecked="⦿" whenUnchecked="⦾" value={3} className="layout-radio"/>
+				<Radio checked={this.state.checked === 0} type="radio" name="layout" whenChecked={Column} whenUnchecked={Column} value={0} className="layout-radio" title="column"/>
+				<Radio checked={this.state.checked === 1} type="radio" name="layout" whenChecked={ColumnWrap} whenUnchecked={ColumnWrap} value={1} className="layout-radio" title="column wrap"/>
+				<Radio checked={this.state.checked === 2} type="radio" name="layout" whenChecked={Row} whenUnchecked={Row} value={2} className="layout-radio" title="row"/>
 			</header>
 		);
 	}
