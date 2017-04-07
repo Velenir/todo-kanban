be714b464aae20a40ea7863da3d58ec7ee1cd2d2
Velenir <Velenir@users.noreply.github.com>
2017-04-07 19:40:05 +0300

    Fix Description not displaying item name

--- app/containers/DescriptionContainer.jsx
+++ app/containers/DescriptionContainer.jsx
@@ -3,8 +3,9 @@ import {changeDescription} from '../actions';
 
 import Description from '../components/Description';
 
-const mapStateToProps = ({modal: {id}, descriptions}) => ({
+const mapStateToProps = ({modal: {id, item}, descriptions}) => ({
 	id,
+	item,
 	description: descriptions.get(id)
 });
 
